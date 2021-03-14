84d2f9a1e746cc0110390e1fe94de562cd8be684
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2021-03-14 15:37:58 -0500

    adds espn bpi crawler

--- crawler.py
+++ crawler.py
@@ -19,14 +19,31 @@ def get_kenpom_rankings():
             except IndexError:
                 continue
 
-    kenpom_write_file(kenpom)
+    write_rankings(kenpom, 'kenpom')
 
 
-def kenpom_write_file(ratings):
-    with open("rankings/kenpom.csv", 'w') as file:
+def write_rankings(ratings, source):
+    with open("rankings/{}.csv".format(source), 'w') as file:
         for team in ratings:
             file.write("{},{},\n".format(team, ratings[team]))
 
 
+def espn_get_rankings():
+    bpi = {}
+    for i in range(1, 8):
+        r = requests.get("https://www.espn.com/mens-college-basketball/bpi/_/view/bpi/page/{}".format(i)).text
+        espn_soup = BeautifulSoup(r, 'lxml')
+
+        bpi_table = espn_soup.find('table', {"class": "bpi__table"}).find('tbody')
+
+        for team in bpi_table.find_all("tr"):
+            team_cols = team.find_all("td")
+            name = team_cols[1].find("span", {"class": "team-names"}).text
+            bpi_rating = float(team_cols[6].text)
+            bpi[name] = bpi_rating
+
+    write_rankings(bpi, 'espn_bpi')
+
 if __name__ == "__main__":
-    get_kenpom_rankings()
+    # get_kenpom_rankings()
+    espn_get_rankings()
